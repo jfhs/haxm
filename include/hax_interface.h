d938351923aecc3269d3bca6fc23dfb579f91b4b
Alexandro Sanchez Bach <alexandro@phi.nz>
2018-07-22 16:22:54 +0200

    Add guest debugging support
    Signed-off-by: Alexandro Sanchez Bach <alexandro@phi.nz>

--- include/hax_interface.h
+++ include/hax_interface.h
@@ -143,6 +143,11 @@ struct hax_tunnel {
         struct {
             paddr_t dummy;
         } state;
+        struct {
+            uint64_t rip;
+            uint64_t dr6;
+            uint64_t dr7;
+        } debug;
     };
     uint64_t apic_base;
 } PACKED;
@@ -267,6 +272,16 @@ struct hax_qemu_version {
     uint32_t cur_version;
     /* The least API version supported by QEMU */
     uint32_t least_version;
+} PACKED;
+
+#define HAX_DEBUG_ENABLE     (1 << 0)
+#define HAX_DEBUG_STEP       (1 << 1)
+#define HAX_DEBUG_USE_SW_BP  (1 << 2)
+#define HAX_DEBUG_USE_HW_BP  (1 << 3)
+
+struct hax_debug_t {
+    uint32_t control;
+    uint64_t dr[8];
 } PACKED;
 
 #endif  // HAX_INTERFACE_H_
